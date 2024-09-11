2c87a97d57cef7857ed7a143eaaa6d15798e83f1
Your Name <you@example.com>
2024-09-11 20:56:15 +0300

    latest variant

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ import java.util.*;
 class FibNumb{
     int ordNum;
     int num;
+    FibNumb(){
+        ordNum = 0;
+        num = 0;
+    }
     FibNumb(int oN, int n){
         oN = ordNum;
         n = num;
@@ -18,22 +22,31 @@ class FibNumb{
 
 class Main{
     public static void main(String[] args) {
-        int N = 0, f1 = 1, f2 = 0, sum = 0;
-        if (args.length > 0)
-            System.out.println("Кількість чисел Фібоначчі: " + args[0]);
-        else {
-            Scanner reader = new Scanner(System.in);
-            System.out.print("Введіть кількість чисел Фібоначчі: ");
-            N = reader.nextInt();
-        }
+        int N, f1 = 1, f2 = 1, sum = 0, tmp;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Введіть кількість чисел Фібоначчі: ");
+        N = reader.nextInt();
+        System.out.println("Введене число: "+ N);
         FibNumb[] fib = new FibNumb[N];
-        for(int i = 0; i < fib.length; i++){
-            fib[i].ordNum = ++i;
-            fib[i].num = f1;
-            sum += f1;
-            i++;
-            ggh
-
+        for (int i = 0; i < N; i++) {
+            fib[i] = new FibNumb();
         }
+        for(int i = 0; i < fib.length; i++){
+            fib[i].ordNum = i + 1;
+            tmp = f1 + f2;
+            fib[i].num = f1;
+            f1 = f2;
+            f2 = tmp;
+        }
+
+        for(int i = 0; i < fib.length; i++){
+            System.out.println(fib[i].getNum());
+        }
+
+        for(int i = 0; i < fib.length; i++){
+            sum += fib[i].getNum();
+        }
+        System.out.println("Сума перших "+ N + " чисел Фібоначчі: " + sum);
+
     }
 }
