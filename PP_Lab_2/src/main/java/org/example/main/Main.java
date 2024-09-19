814441c2fe0f84d3b4c4895046befb0d77f7e9af
TetianaMeshk <tetiana.meshkova.oi.2023@lpnu.ua>
2024-09-19 20:57:01 +0300

    2 lab last version

--- PP_Lab_2/src/main/java/org/example/main/Main.java
+++ PP_Lab_2/src/main/java/org/example/main/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     /**
-     * метод для створення масиву об'єктів
+     * Метод для створення масиву об'єктів
      * @param n кількість елементів масиву
      * @return arr - масив об'єктів класу Customer
      */
@@ -19,23 +19,25 @@ public class Main {
                 System.out.print("ID: ");
                 if(sc.hasNextInt()){
                     id = sc.nextInt();
-                    if(id>0)
+                    if(id > 0)
                         break;
                     else{
-                        System.out.println("Помилка: id має бути ненульовим");
+                        System.out.println("Помилка: id має бути ненульовим і додатним");
                     }
                 }
                 else{
                     System.out.println("Помилка: введіть ціле число");
-                    sc.nextLine(); // Очищуємо буфер після введення числа
+                    sc.next(); // Очищуємо буфер
                 }
             }
+
+            sc.nextLine(); // Очищуємо буфер після введення числа
 
             String lastName;
             while (true){
                 System.out.print("Прізвище: ");
-                lastName = sc.next();
-                if(lastName.matches("[А-Яа-яїЇ]+")){
+                lastName = sc.nextLine();
+                if(lastName.matches("[a-zA-Zа-яА-ЯіІїЇєЄЙй']+")){
                     break;
                 }
                 else{
@@ -46,8 +48,8 @@ public class Main {
             String firstName;
             while (true){
                 System.out.print("Ім'я: ");
-                firstName = sc.next();
-                if(firstName.matches("[А-Яа-яїЇ]+")){
+                firstName = sc.nextLine();
+                if(firstName.matches("[a-zA-Zа-яА-ЯіІїЇєЄЙй']+")){
                     break;
                 }
                 else{
@@ -58,8 +60,8 @@ public class Main {
             String middleName;
             while (true){
                 System.out.print("По-батькові: ");
-                middleName = sc.next();
-                if(middleName.matches("[А-Яа-яїЇ]+")){
+                middleName = sc.nextLine();
+                if(middleName.matches("[a-zA-Zа-яА-ЯіІїЇєЄЙй']+")){
                     break;
                 }
                 else{
@@ -68,24 +70,23 @@ public class Main {
             }
 
             System.out.print("Адреса: ");
-            String address = sc.next();
-            sc.nextLine();
+            String address = sc.nextLine();
 
             long cardNum;
             while (true){
                 System.out.print("Номер картки: ");
                 if(sc.hasNextLong()){
                     cardNum = sc.nextLong();
-                    if(cardNum>0){
+                    if(cardNum > 0){
                         break;
                     }
                     else{
-                        System.out.println("Помилка: номер картки має бути ненульовим");
+                        System.out.println("Помилка: номер картки має бути додатним");
                     }
                 }
                 else{
                     System.out.println("Помилка: введіть ціле число");
-                    sc.nextLine(); // Очищуємо буфер після введення числа
+                    sc.next(); // Очищуємо буфер
                 }
             }
 
@@ -100,28 +101,29 @@ public class Main {
                 }
                 else{
                     System.out.println("Помилка: введіть числове значення");
-                    sc.nextLine(); // Очищуємо буфер після введення числа
+                    sc.next(); // Очищуємо буфер
                 }
             }
 
             arr[i] = new Customer(id, lastName, firstName, middleName, address, cardNum, cardBalance);
+            sc.nextLine(); // Очищуємо буфер після введення значень
         }
         return arr;
     }
 
     /**
-     * метод для пошуку і виведення клієнтів з заданим іменем
+     * Метод для пошуку і виведення клієнтів з заданим іменем
      * @param arr масив об'єктів класу Customer
      * @param fName ім'я - критерій пошуку і виведення об'єктів
      */
     public static void findCustByFName(Customer[] arr, String fName) {
-        int j = 1;
+        int j = 0;
         for (Customer customer : arr) {
             if (customer.getFirstName().equalsIgnoreCase(fName))
                 j++;
         }
 
-        if (j > 1) {
+        if (j > 0) {
             System.out.println("Клієнти з іменем " + fName + ":");
             j = 1;
             for (Customer customer : arr) {
@@ -131,25 +133,25 @@ public class Main {
                 }
             }
         }
-        else if (j == 1) {
+        else {
             System.out.println("Немає клієнтів з іменем " + fName);
         }
     }
 
     /**
-     * метод для виведення клієнтів з № картки в заданих межах
+     * Метод для виведення клієнтів з № картки в заданих межах
      * @param arr масив об'єктів класу Customer
      * @param k1 нижня межа діапазону
      * @param k2 верхня межа діапазону
      */
     public static void findCustByCard(Customer[] arr, long k1, long k2) {
-        int j = 1;
+        int j = 0;
         for (Customer customer : arr) {
             if (customer.getCardNum() > k1 && customer.getCardNum() < k2)
                 j++;
         }
 
-        if(j > 1) {
+        if(j > 0) {
             System.out.println("\nКлієнти з картками в заданих межах: ");
             j = 1;
             for (Customer customer : arr) {
@@ -159,13 +161,13 @@ public class Main {
                 }
             }
         }
-        if (j == 1) {
+        else {
             System.out.println("Немає клієнтів з карткою в заданому інтервалі");
         }
     }
 
     /**
-     * метод для виведення кількості клієнтів з заборгованостями
+     * Метод для виведення кількості клієнтів з заборгованостями
      * і їхньої інформації
      * @param arr масив об'єктів класу Customer
      */
@@ -180,7 +182,6 @@ public class Main {
         if (count > 0) {
             System.out.println("\nКількість клієнтів з заборгованістю: " + count);
             System.out.println("Клієнти з заборгованістю:");
-            count = 0;
             for (Customer customer : arr) {
                 if (customer.getCardBalance() < 0) {
                     System.out.println(j + ". " + customer);
@@ -199,7 +200,7 @@ public class Main {
             System.out.print("Введіть кількість клієнтів банку: ");
             if(sc.hasNextInt()){
                 n = sc.nextInt();
-                if(n>0)
+                if(n > 0)
                     break;
                 else{
                     System.out.println("Помилка: кількість має бути ненульовою");
@@ -207,14 +208,26 @@ public class Main {
             }
             else{
                 System.out.println("Помилка: введіть ціле число");
-                sc.nextLine(); // Очищуємо буфер після введення числа
+                sc.next(); // Очищуємо буфер
             }
         }
 
+        sc.nextLine(); // Очищуємо буфер після введення числа
         Customer[] customers = createArr(n);
 
-        System.out.print("\nВведіть ім'я клієнта для пошуку: ");
-        findCustByFName(customers, sc.nextLine());
+
+        String fName;
+        while (true){
+            System.out.print("\nВведіть ім'я клієнта для пошуку: ");
+            fName = sc.nextLine();
+            if(fName.matches("[a-zA-Zа-яА-ЯіІїЇєЄЙй']+")){
+                break;
+            }
+            else{
+                System.out.println("Помилка: ім'я повинне містити тільки букви");
+            }
+        }
+        findCustByFName(customers, fName);
 
         System.out.println("\nВведіть межі для номера картки клієнта: ");
         System.out.print("Введіть 1 межу: ");
